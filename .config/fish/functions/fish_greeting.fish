5db4753d07631bc207082ccc035e003b311ebaab
Aylon Ben Dvora <70889286+I-ABD-I@users.noreply.github.com>
2025-06-26 20:03:13 +0300

    added ghostty and nvim config, modified fish greeting and added vim as an alias to nvim, changed ashell second monitor color

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,3 +1,10 @@
 function fish_greeting
-	fastfetch
+
+	begin
+		sleep 0.01
+		# run fastfetch only if shell is big enoght
+		if test (string split ' ' (stty size))[2] -ge 94 -a (string split ' ' (stty size))[1] -ge 23
+			fastfetch
+		end
+	end &
 end
---
+++ .config/nvim/init.lua
@@ -0,0 +1,3 @@
+vim.opt.mouse = ""
+vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-v>', '"+P', { noremap = true, silent = true })
